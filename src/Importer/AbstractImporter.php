3c8ad6410a9cfe330451d1df78111c55a026897f
Dmytro Feshchenko <dmytro.feshchenko@gmail.com>
2021-01-09 13:01:08 +0200

    Small update

--- src/Importer/AbstractImporter.php
+++ src/Importer/AbstractImporter.php
@@ -518,7 +518,7 @@ abstract class AbstractImporter implements ImporterInterface
     protected function importTaskPage($task, int $page, array $options = []): bool
     {
         $link = $task->getPreparedLink($page);
-        $this->getOutput()->section(sprintf('Reading data from resource %s', $task->isPaginatedLink() ? $page : ''));
+        $this->getOutput()->section(sprintf('Reading data from resource %s', $task->isPaginatedLink() ? '(Page '.$page.')' : ''));
         $this->getLogger()->info(sprintf('Reading data from resource %s: START', $link));
 
         $data = $task->getDataFromLink(false, $link);
